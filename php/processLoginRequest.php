e5e0508cfb76f6099e3a9588aeaa5a2a40a03445
Sameer Balasubrahmanyam <bs.sameer1@gmail.com>
2013-08-18 15:29:46 +0530

    register complete

--- php/processLoginRequest.php
+++ php/processLoginRequest.php
@@ -9,6 +9,31 @@
 $email= $_GET["email"];
 $password = $_GET["password"];
 
+
+ try {
+                require_once 'conf.php';
+                $conn = mysqlConnector();
+                $query="SELECT userPassword from user_data "
+
+
+        } catch(PDOException $e) {
+                echo 'ERROR: ' . $e->getMessage();
+        }
+
+
+if($targetPassword = $conn->query($query) or die($conn->error)) {
+		if (sha1($password)== $targetPassword) {
+			echo " Authentication Successful! Redirecting.... ";
+		}
+		
+               
+}else {
+	echo "Sorry wrong username and/or password";
+}
+
+
+
+
  ?>
 </body>
 </html>
