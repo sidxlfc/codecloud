d9c1b24a8c3a3f65f1956e1ab86dbe5b9cb42ba0
Sameer Balasubrahmanyam <bs.sameer1@gmail.com>
2013-08-18 17:00:53 +0530

    login setup

--- php/processLoginRequest.php
+++ php/processLoginRequest.php
@@ -17,13 +17,14 @@ $passwordHash = sha1($password);
         $conn = mysqlConnector();
         $query="SELECT userPassword FROM user_data WHERE userEmail='$email' ";
 		$result= $conn -> query ($query) or die ($mysql -> error);
-
+		echo "call to result ";
        	if($result) {
         while($row = $result->fetch_object()) {
         $userPassword = $row-> userPassword;
         echo $userPassword;
 
         }
+        echo $result;
     }
     else
     	{
