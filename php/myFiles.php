1f91519671be3e71f0beb880c8379db56cf4448f
Sameer Balasubrahmanyam <bs.sameer1@gmail.com>
2013-08-18 19:10:39 +0530

    login setup

--- php/myFiles.php
+++ php/myFiles.php
@@ -6,7 +6,7 @@
 
 session_start();
 $email = $_SESSION['sessionVar'];
-$dir    = '$OPENSHIFT_DATA_DIR/ $email/';
+$dir    = '$OPENSHIFT_DATA_DIR/'.$email;
 echo $dir;
 $files1 = scandir($dir);
 $files2 = scandir($dir, 1);
