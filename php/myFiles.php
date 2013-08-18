72dfe3bedec6f3b953fe9061bc06e88e693eefd3
Sameer Balasubrahmanyam <bs.sameer1@gmail.com>
2013-08-18 18:42:14 +0530

    login setup

--- php/myFiles.php
+++ php/myFiles.php
@@ -6,7 +6,19 @@
 
 session_start();
   $email = $_SESSION['sessionVar'];
-  echo "$email";
+  if ($handle = opendir('/var/lib/openshift/52106d8ce0b8cd5b44000013/app-root/runtime/repo/php/userData/$email')) {
+    echo "Directory handle: $handle\n";
+    echo "Entries:\n";
+
+    /* This is the correct way to loop over the directory. */
+    while (false !== ($entry = readdir($handle))) {
+        echo "$entry\n";
+    }
+
+    
+
+    closedir($handle);
+}
 ?>
 </head>
 <body>
