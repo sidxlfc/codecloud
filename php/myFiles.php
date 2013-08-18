a000618a2fab7144dc625195af7ab61ad123bb8c
Sameer Balasubrahmanyam <bs.sameer1@gmail.com>
2013-08-18 18:58:51 +0530

    login setup

--- php/myFiles.php
+++ php/myFiles.php
@@ -7,6 +7,7 @@
 session_start();
 $email = $_SESSION['sessionVar'];
 $dir    = '/var/lib/openshift/52106d8ce0b8cd5b44000013/app-root/runtime/repo/php/userData/$email/';
+echo $dir;
 $files1 = scandir($dir);
 $files2 = scandir($dir, 1);
 
