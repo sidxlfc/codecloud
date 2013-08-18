7f7e9c2778c2279125576477f60626fc61510337
Sameer Balasubrahmanyam <bs.sameer1@gmail.com>
2013-08-18 13:50:50 +0530

    fixed register bug

--- php/processRegisterRequest.php
+++ php/processRegisterRequest.php
@@ -14,7 +14,7 @@ $passwordHash=sha1($password);
 if ($password !== $confirmPassword)
 	echo "Passwords do not match kindly go back and re enter.";
 else
-$mysql = new mysqli('localhost', 'root', 'thisismylife', 'code_cloud') or die('you\'re dead');
+$mysql = new mysqli('mysql://$OPENSHIFT_MYSQL_DB_HOST:$OPENSHIFT_MYSQL_DB_PORT/', 'adminxl5QcED', 'Qim_GuYLD2a9', 'codecloud') or die('you\'re dead');
 $result= $mysql -> query (" INSERT INTO user_data(userEmail,userPassword) VALUES('$email' , '$passwordHash' )  " ) or die ($mysql -> error);
 mkdir("~/$email",0777,true);
 
