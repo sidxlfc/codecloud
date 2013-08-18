ae5beb56658d07eb4ac4995570f008566cecbdc4
Sameer Balasubrahmanyam <bs.sameer1@gmail.com>
2013-08-18 14:33:05 +0530

    test tool 1

--- php/test.php
+++ php/test.php
@@ -2,24 +2,6 @@
  try {
                 require_once 'conf.php';
                 $conn = mysqlConnector();
-                $stmt = $conn->prepare('SELECT * FROM demo');
-                $stmt->execute();
-
-                while($row = $stmt->fetch()) {
-                        print_r($row);          
-                }
-
-
-        } catch(PDOException $e) {
-                echo 'ERROR: ' . $e->getMessage();
-        }
-
-
-?>
-<?php
- try {
-                require_once 'conf.php';
-                $conn = mysqlConnector();
                 $stmt = $conn->prepare('SELECT * FROM user_data ');
                 $stmt->execute();
 
