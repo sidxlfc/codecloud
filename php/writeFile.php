4d3fc3d5595612b67ca2471562c7a4b06a5e00dc
Sameer Balasubrahmanyam <bs.sameer1@gmail.com>
2013-08-27 19:28:22 +0530

    read file

--- php/writeFile.php
+++ php/writeFile.php
@@ -8,7 +8,7 @@ session_start();
 $email = $_SESSION['sessionVar'];
 
 $filePath='/var/lib/openshift/52106d8ce0b8cd5b44000013/app-root/data/'.$email."/".$fileName;
-$handle=fopen($filePath,"+r");
+$handle=fopen($filePath,"w+");
 $x=fwrite($handle,$fileData);
 
 
