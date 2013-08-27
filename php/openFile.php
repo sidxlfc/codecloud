7eb37c457df4b362ecaa86d645021a98a5448bfa
Sameer Balasubrahmanyam <bs.sameer1@gmail.com>
2013-08-27 19:38:17 +0530

    read file

--- php/openFile.php
+++ php/openFile.php
@@ -24,7 +24,7 @@ session_start();
 $fileName=$_GET['fileName'];
 $email = $_SESSION['sessionVar'];
 $filePath=  '/var/lib/openshift/52106d8ce0b8cd5b44000013/app-root/data/'.$email."/".$fileName;
-$file = fopen($filePath, "w+") or exit("Unable to open file!");
+$file = fopen($filePath, "a+") or exit("Unable to open file!");
 
 while(!feof($file))
   {
