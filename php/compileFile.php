0d951388d06bafc3f514eae051f2019109a819e7
Sameer Balasubrahmanyam <bs.sameer1@gmail.com>
2013-08-30 17:06:42 +0530

    compile files

--- php/compileFile.php
+++ php/compileFile.php
@@ -3,8 +3,8 @@ $fileName=$_POST['fileName'];
 session_start();
 $email = $_SESSION['sessionVar'];
 $filePath=  '/var/lib/openshift/52106d8ce0b8cd5b44000013/app-root/data/'.$email."/".$fileName;
+echo $filePath;
 $output=shell_exec('gcc -g -O -c '.$filePath);
 echo $output;
-header("Location: myFiles.php");
-die();
+
 ?>
