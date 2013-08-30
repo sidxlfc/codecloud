b258ca9f48fcd525357a5b0cfc19df967eb6305b
Sameer Balasubrahmanyam <bs.sameer1@gmail.com>
2013-08-30 18:06:14 +0530

    compile files

--- php/compileFile.php
+++ php/compileFile.php
@@ -1,14 +1,14 @@
 <?php
-/*$fileName=$_POST['fileName'];
+$fileName=$_POST['fileName'];
+$objectFileName= str_replace('.c', '.o' , $fileName);
+echo $objectFileName;
 session_start();
 $email = $_SESSION['sessionVar'];
 $filePath=  '/var/lib/openshift/52106d8ce0b8cd5b44000013/app-root/data/'.$email."/".$fileName;
 echo $filePath;
-$output= exec('gcc -g -O -c '.$filePath);
-echo $output;
-*/
-	#$output=exec('gcc -g -O -c '.$filePath);
-	exec('gcc -c /var/lib/openshift/52106d8ce0b8cd5b44000013/app-root/data/bs.sameer1@gmail.com/Hello.c  -o /var/lib/openshift/52106d8ce0b8cd5b44000013/app-root/data/bs.sameer1@gmail.com/something.o ',$output,$return);
+$command='gcc -c $filePath -o /var/lib/openshift/52106d8ce0b8cd5b44000013/app-root/data/'.$email."/".$objectFileName;
+echo $command;
+exec($command,$output,$return);
 	echo $output;
 	echo $return;
 ?>
