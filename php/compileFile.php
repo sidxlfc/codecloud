1edda34a12569e08d25dcd8aba093e7feb8a1343
Sameer Balasubrahmanyam <bs.sameer1@gmail.com>
2013-09-01 01:49:02 +0530

    output formatting

--- php/compileFile.php
+++ php/compileFile.php
@@ -34,19 +34,14 @@ else
 {
 	$objectFileName= str_replace('.cpp', '.o' , $fileName);
 }
-
-echo $objectFileName;
 session_start();
 $email = $_SESSION['sessionVar'];
 $sourceFilePath= '/var/lib/openshift/52106d8ce0b8cd5b44000013/app-root/data/'.$email."/".$fileName;
 $objectFilePath= '/var/lib/openshift/52106d8ce0b8cd5b44000013/app-root/data/'.$email."/".$objectFileName;
-#echo $filePath;
 $command='gcc -c '.$sourceFilePath.' -o '.$objectFilePath. ' 2>&1 ';
-#echo $command;
 $output=shell_exec($command);
 echo str_replace("/var/lib/openshift/52106d8ce0b8cd5b44000013/app-root/data/".$email."/", "<br>" , $output);
 
-	
 $file = $objectFilePath;
 
 if (file_exists($file)) {
