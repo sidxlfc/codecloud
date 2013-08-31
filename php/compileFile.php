dcb02a8da1c434043b5163effdf32d7fad9e7ab0
Sameer Balasubrahmanyam <bs.sameer1@gmail.com>
2013-09-01 01:02:28 +0530

    output formatting

--- php/compileFile.php
+++ php/compileFile.php
@@ -31,7 +31,7 @@ $objectFilePath= '/var/lib/openshift/52106d8ce0b8cd5b44000013/app-root/data/'.$e
 $command='gcc -c '.$sourceFilePath.' -o '.$objectFilePath. ' 2>&1 ';
 #echo $command;
 $output=shell_exec($command);
-echo $output;
+#echo $output;
 
 	
 $file = $objectFilePath;
