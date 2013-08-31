d3577f4d2a7d934abaad6d3bcc859505356a32cb
Sameer Balasubrahmanyam <bs.sameer1@gmail.com>
2013-09-01 00:31:30 +0530

    output formatting

--- php/compileFile.php
+++ php/compileFile.php
@@ -40,7 +40,7 @@ if (file_exists($file)) {
 	echo $output;
     header('Content-Description: File Transfer');
     header('Content-Type: application/octet-stream');
-    header('Content-Disposition: attachment; filename='.basename($file));
+    header('Content-Disposition: attachment; filename='.$objectFileName);
     header('Content-Transfer-Encoding: binary');
     header('Expires: 0');
     header('Cache-Control: must-revalidate');
