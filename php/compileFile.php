b788fb0e0d3a80ca55e87585766db8cae24cd10e
Sameer Balasubrahmanyam <bs.sameer1@gmail.com>
2013-08-31 23:46:01 +0530

    output formatting

--- php/compileFile.php
+++ php/compileFile.php
@@ -26,7 +26,7 @@ $email = $_SESSION['sessionVar'];
 $sourceFilePath=  '/var/lib/openshift/52106d8ce0b8cd5b44000013/app-root/data/'.$email."/".$fileName;
 $objectFilePath= '/var/lib/openshift/52106d8ce0b8cd5b44000013/app-root/data/'.$email."/".$objectFileName;
 #echo $filePath;
-$command='gcc -c '.$sourceFilePath.' -o '.$objectFilePath;
+$command='gcc -v -c '.$sourceFilePath.' -o '.$objectFilePath;
 #echo $command;
 exec($command,$output,$return);
 print_r($output) ;
