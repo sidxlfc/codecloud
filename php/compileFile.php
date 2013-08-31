5f7c89f6cdb2e3a024f64389570746ffbc8afbce
Sameer Balasubrahmanyam <bs.sameer1@gmail.com>
2013-09-01 00:27:21 +0530

    output formatting

--- php/compileFile.php
+++ php/compileFile.php
@@ -46,7 +46,7 @@ if (file_exists($file)) {
     header('Cache-Control: must-revalidate');
     header('Pragma: public');
     header('Content-Length: ' . filesize($file));
-    ob_clean();
+    #ob_clean();
     #flush();
     readfile($file);
     exit;
